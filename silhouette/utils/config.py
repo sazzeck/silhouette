8f1ebfaca17e392a75d07437f66cab7869db8ec5
sazzeck <100031065+sazzeck@users.noreply.github.com>
2022-08-10 00:01:05 +0300

    basic backend settings.

---
+++ silhouette/utils/config.py
@@ -0,0 +1,30 @@
+import typing
+from functools import lru_cache
+from os import environ
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class ConfigMeta(type):
+    def resolve_value(cls, value: str):
+        _map: typing.Dict[str, typing.Callable[[str], typing.Any]] = {
+            "bool": bool,
+            "int": int,
+            "float": float,
+            "file": lambda x: Path(x).read_text().strip("\n"),
+            "str": str,
+            "list": lambda x: [cls.resolve_value(y) for y in x.split(",")],
+        }
+
+        return _map[(v := value.split(":", maxsplit=1))[0]](v[1])
+
+    @lru_cache()
+    def __getattr__(cls, name):
+        return cls.resolve_value(environ[name])
+
+
+class Config(metaclass=ConfigMeta):
+    pass
